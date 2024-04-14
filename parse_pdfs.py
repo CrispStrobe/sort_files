aae38ed2849f23a4e13aaf81cc56af385b189e08
CrispStrobe <154636388+CrispStrobe@users.noreply.github.com>
2024-04-14 21:27:16 +0200

    Update parse_pdfs.py

--- parse_pdfs.py
+++ parse_pdfs.py
@@ -4,13 +4,10 @@ import re
 from PyPDF2 import PdfReader
 from pdf2image import convert_from_path
 import pytesseract
-#from PIL import Image
 from openai import OpenAI
 from tqdm import tqdm
 
 modelname="cas/spaetzle-v58:latest"
-#model="cas/occiglot-7b-de-en-instruct-q4-k-m",
-#modelname = "mistral:7b-instruct-q4_K_M"
 
 def extract_text_from_pdf(pdf_path):
     text = ""
@@ -34,48 +31,72 @@ def perform_ocr(pdf_path):
             break
     return text[:2000]
 
-def sort_author_names(author_names):
+def valid_author_name(author_names):
+    """ Check if the author names are valid, i.e., each name has at least two words. """
+    #return all(len(name.split()) > 1 for name in author_names.split(", "))
+    """Check if the author name is valid, i.e., contains at least a first name and a last name."""
+    return len(author_names.strip().split()) > 1
+
+def sort_author_names(author_names, attempt=1):
+    if attempt > 5:
+        print("Maximum retry attempts reached for sorting names.")
+        return author_names
+    #print ("\nsorting author names:", author_names)
     client = OpenAI(
         base_url="http://localhost:11434/v1/",
         api_key="ollama"
     )
-    # Building a proper prompt for the OpenAI API
-    prompt = ("Please reorder the following author names into the format 'Lastname Surname', if they are not already. "
-              "Each name should be in the format <AUTHOR>Lastname Surname</AUTHOR>. Here are the names: " +
-              ' '.join(f"<AUTHOR>{name}</AUTHOR>" for name in author_names.split(", ")))
-    
+
+    prompt = (f"You will be given an author name that you must put into the format 'Lastname Surname'."
+              f"So, you must first make an educated guess if the given input is already in this format. If so, return it back."
+              f"If not and it is more plausibly in the format 'Surname(s) Lastname', you must reformat it."
+              f"Example: 'Jan Christian Gertz' must become 'Gertz Jan Christian' and 'Michael M. Meier' must become 'Meier Michael M'."
+              f"No comma after the Lastname!"
+              f"If you are given multiple person names, only keep the first and omit all others."
+              f"You must give the output in the format: <AUTHOR>Lastname Surname(s)</AUTHOR>."
+              f"Here are the name parts: <AUTHOR>{author_names}</AUTHOR>")
+
     messages = [{"role": "user", "content": prompt}]
+    #print ("sorting prompt:", messages)
     response = client.chat.completions.create(
         model=modelname,
         temperature=0.5,
-        max_tokens=500,  # Adjusted to allow for larger responses
+        max_tokens=500,
         messages=messages
     )
     
-    # Extracting and reformating the response to only include names
-    reformatted_names = response.choices[0].message.content.strip()
-    
-    # Parsing the output to ensure it is in the correct format
-    # This regex extracts text within <AUTHOR> tags, assuming the API follows the instructions properly
-    names = re.findall(r'<AUTHOR>(.*?)</AUTHOR>', reformatted_names)
-    
-    # Joining names with a comma if multiple authors are present
-    ordered_names = ", ".join(names)
-    #print ("\nordered names:", ordered_names)
-    return ordered_names
+    reformatted_name = response.choices[0].message.content.strip()
+    #print ("\nmodel output:", reformatted_name)
+    #names = re.findall(r'<AUTHOR>(.*?)</AUTHOR>', reformatted_names)
+    name = re.search(r'<AUTHOR>(.*?)</AUTHOR>', reformatted_name)
+    #print ("\nonly the name part:", name) 
+    #ordered_names = ", ".join(names)
+    #print ("\sorted author names:", ordered_names)
+    #if "lastname" in ordered_names.lower() or "surname" in ordered_names.lower() or not valid_author_name(ordered_names):
+    #    print("\nRetrying author names sorting due to detection of placeholder names or invalid format: ", ordered_names)
+    #    return sort_author_names(author_names, attempt + 1)
+    #return ordered_names
+    if name:
+        ordered_name = name.group(1)
+        #print ("ordered name:", ordered_name)
+        if not valid_author_name(ordered_name):
+            print(f"Invalid author name format detected: '{ordered_name}', retrying...")
+            return sort_author_names(author_names, attempt + 1)
+        return ordered_name
+    else:
+        print("Failed to extract a valid name, retrying...")
+        return sort_author_names(author_names, attempt + 1)
 
-
-def send_to_ollama_server(text, attempt=1):
+def send_to_ollama_server(text, filename, attempt=1):
     client = OpenAI(
         base_url="http://localhost:11434/v1/",
         api_key="ollama"
     )
-    prompt = ("Extract the author name, year of publication, and title from the following text which is an "
-              "OCR extract from an academic publication. I need the output in the following format: "
+    base_filename = os.path.splitext(os.path.basename(filename))[0]
+    prompt = (f"Extract the first author nam (ignore other authors), year of publication, and title from the following text, considering the filename '{base_filename}' which may contain clues. "
+              "I need the output in the following format: "
               "<TITLE>The publication title</TITLE>, <YEAR>2023</YEAR>, <AUTHOR>Lastname Surname</AUTHOR>. "
-              "You must make very sure you have the correct order of Lastname Surname. If your result looks more like Surname Lastname, reorder the names!"
-              "If there are several authors, specify the first two (omitting others, if any) like this: <AUTHOR>Lastname Surname, Lastname Surname</AUTHOR>"
-              "Now here is the extracted text:\n" + text)
+              "Here is the extracted text:\n" + text)
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model = modelname,
@@ -88,8 +109,8 @@ def send_to_ollama_server(text, attempt=1):
     #response.choices[0].message.content.strip()
     #print ("\noutput: ", output)
 
-    if "Lastname Surname" in output and attempt < 3:
-        return send_to_ollama_server(text, attempt + 1)
+    if ("lastname" in output.lower() or "surname" in output.lower()) and attempt < 4:
+        return send_to_ollama_server(text, filename, attempt + 1)
     return output
 
 def parse_metadata(content):
@@ -106,6 +127,9 @@ def parse_metadata(content):
     year = year_match.group(1)
     author = author_match.group(1)
 
+    if "unknown" in (title.lower(), year.lower(), author.lower()) or len(author.split()) <= 1:
+        return None
+    
     if "lastname" in author.lower() or "surname" in author.lower():
         return None  # Check for placeholder text in author names
 
@@ -116,39 +140,57 @@ def parse_metadata(content):
 def sanitize_filename(name):
     return re.sub(r'[\\/*?:"<>|]', "", name)
 
-def process_pdf(pdf_path):
+def process_pdf(pdf_path, attempt=1):
+    if attempt > 4:  # Limiting the retries to prevent infinite loops
+        print(f"Maximum retry attempts reached for {pdf_path}.")
+        return None
+
     text = extract_text_from_pdf(pdf_path)
     if not text.strip():
         text = perform_ocr(pdf_path)
 
-    metadata_content = send_to_ollama_server(text)
+    metadata_content = send_to_ollama_server(text, pdf_path, attempt)
     metadata = parse_metadata(metadata_content)
-    if metadata and 'author' in metadata:
-            corrected_authors = sort_author_names(metadata['author'])
-            metadata['author'] = corrected_authors
-            return metadata
+    if metadata:
+        corrected_authors = sort_author_names(metadata['author'])
+        metadata['author'] = corrected_authors
+        # After sorting, check if the author's name still has one word only
+        if len(metadata['author'].split()) <= 1:
+            print(f"Author's name still has insufficient details after sorting, retrying for {pdf_path}. Attempt {attempt}")
+            return process_pdf(pdf_path, attempt + 1)
+        return metadata
     else:
-        print(f"Error: Metadata parsing failed or incomplete for {pdf_path}. Metadata content: {metadata_content}")
-        return None
+        print(f"Error: Metadata parsing failed or incomplete for {pdf_path}. Metadata content: {metadata_content}, retrying... Attempt {attempt}")
+        return process_pdf(pdf_path, attempt + 1)
 
 def main(directory):
-    for filename in tqdm(os.listdir(directory)):
-        if filename.endswith(".pdf"):
-            pdf_path = os.path.join(directory, filename)
-            try:
-                metadata = process_pdf(pdf_path)
-                if metadata:
-                    sanitized_name = sanitize_filename(f"{metadata['author']} {metadata['year']} {metadata['title']}")
-                    #print ("\nnew name: ", sanitized_name)
-                    with open("rename_commands.sh", "a") as mv_file:
-                        mv_file.write(f"mv \"{pdf_path}\" \"{sanitized_name}.pdf\"\n")
-                else:
-                    with open("unparseables.lst", "a") as unparseable_file:
+    if not os.path.exists(directory):
+        print("The specified directory does not exist")
+        sys.exit(1)
+
+    with open("rename_commands.sh", "w") as mv_file, open("unparseables.lst", "w") as unparseable_file:
+        mv_file.write("#!/bin/sh\n")
+        mv_file.flush()
+        for filename in tqdm(os.listdir(directory)):
+            if filename.endswith(".pdf"):
+                pdf_path = os.path.join(directory, filename)
+                try:
+                    metadata = process_pdf(pdf_path)
+                    if metadata:
+                        first_author = metadata['author'].split(", ")[0]
+                        target_dir = os.path.join(directory, first_author)
+                        new_file_path = os.path.join(target_dir, f"{metadata['year']} {sanitize_filename(metadata['title'])}.pdf")
+                        
+                        mv_file.write(f"mkdir -p \"{target_dir}\"\n")
+                        mv_file.write(f"mv \"{pdf_path}\" \"{new_file_path}\"\n")
+                        mv_file.flush()
+                    else:
                         unparseable_file.write(f"{pdf_path}\n")
-            except Exception as e:
-                print(f"Failed to process {pdf_path}: {e}")
-                with open("unparseables.lst", "a") as unparseable_file:
+                        unparseable_file.flush()
+                except Exception as e:
+                    print(f"Failed to process {pdf_path}: {e}")
                     unparseable_file.write(f"{pdf_path}\n")
+                    unparseable_file.flush()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
